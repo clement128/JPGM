291835830ec44713ff363eae11937f5829661149
clement <tanclement128@gmail.com>
2023-03-17 14:22:35 +0800

    add spin api

--- Program.cs
+++ Program.cs
@@ -6,3 +6,21 @@ WriteLine("Game ID: {0}, {1}", game1.Id, game1.Spin(1));
 
 var game2 = new Gemlinks();
 WriteLine("Game ID: {0}, {1}, {2}", game2.Id, game2.Spin(1), game2.Jackpot(1));
+
+
+
+
+(int, int) SpinApi(IGameModule<int, int> game)
+{
+    var spinResult = game.Spin(Random.Shared.Next());
+    var jackPotResult = 0;
+    if (game is IJackpotGameModule<int, int, int> jackpot)
+    {
+        jackPotResult = jackpot.Jackpot(Random.Shared.Next());
+    }
+    return (spinResult, jackPotResult);
+}
+
+
+SpinApi(game1);
+SpinApi(game2);
